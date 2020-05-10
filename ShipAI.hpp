8c22d5ccfb9ce2dbab4e08863eed18b67a468c1a
Becher <mbelkhenchir@gmail.com>
2020-05-10 23:13:33 +0200

    comment shipai

--- ShipAI.hpp
+++ ShipAI.hpp
@@ -79,71 +79,66 @@ namespace shipBTree{
 
 			unique_ptr<GameMap>& game_map = game->game_map;
 			
-			// Si le vaisseau à suffisement de ressources	
+			// Si le vaisseau à suffisement de ressources pour entreprendre un retour
             if (ship->halite >= 800 * game->turn_number/constants::MAX_TURNS ||
+				// Tant que le vaisseau a encore assez de ressources
 				(ships_infos[ship->id].dropoff && ship->halite >= 500 * game->turn_number / constants::MAX_TURNS)) {
 				return _return_dropoff();
 			}
+			// Sinon le vaisseau va récolter de l'halite
 			else {
 			   ships_infos[ship->id].dropoff = false;
 				return _search_halite();
 			}
 
-			return _recolt();
 
 		}
 
 
-
+		// Recherche le dropoff / shipyard le plus proche
 		Position NearDropOff() {
-
-
+			// Position du shipyard
 			Position nearDropOff = game->players[ship->owner]->shipyard->position;
+			int nearDistance = game->game_map->calculate_distance(nearDropOff, ship->position);
 
 			shared_ptr<Player> player = game->players[ship->owner];
 
-
-
+			// Position de chaque dropoff
 			for (int i = 0; i < dropoffs.size(); ++i) {
-				if (game->game_map->calculate_distance(dropoffs[i],ship->position) < game->game_map->calculate_distance(nearDropOff, ship->position)) {
+				int actualDist = game->game_map->calculate_distance(dropoffs[i], ship->position);
+				if (actualDist < nearDistance ) {
 					nearDropOff = dropoffs[i];
-
 				}
-
-				
 			}
-
 			return nearDropOff;
 
 		}
 
 
-
+		// ACTION DE RETOUR AU DROPOFF
 		Command _return_dropoff() {
+			shared_ptr<Player> player = game->me;
+
+			// Si le vaisseau décide de rentrer on cherche le dropoff le plus proche (une seul fois)
 			if (!ships_infos[ship->id].dropoff) {
 				ships_infos[ship->id].position = NearDropOff();
 			}
-
 			ships_infos[ship->id].dropoff = true;
 
+			// Si il est plus rentable de rester sur la case
 			if (game_map->at(ship)->halite > constants::MAX_HALITE / 10 && !ship->is_full()) return ship->stay_still();
 
-			
-			
-
-			shared_ptr<Player> player = game->players[ship->owner];
-
-			
+			// Si le dropoff le plus proche est très loin et que il n'y a pas de dropoff ennemi à proximitée
 			if (player->dropoffs.size() < 2 && !dropoff && game->players[ship->owner]->halite > constants::DROPOFF_COST && game_map->calculate_distance(ships_infos[ship->id].position, ship->position) > 15
 				&& (enemyDropoffInRange(ship->position, 4, player->id, game_map).size() ==  0)) {
+				// informe les autres vaisseaux qu'il y'a déja un dropoff créé ce tour ci 
 				dropoff = true;
 
 				dropoffs.push_back(ship->position);
 				return ship->make_dropoff();
 			}
 			
-			
-
+			// Regarde si le calcul de direction n'échoue pas, le vaisseau va vers le dropoff. sinon, il va chercher de l'halite en attendant
 			Direction d = game_map->naive_navigate(ship, ships_infos[ship->id].position);
 			if (d == Direction::STILL) return _search_halite();
 			return ship->move(d);
@@ -154,20 +149,23 @@ namespace shipBTree{
 
 
 			   
-
+		// ACTION DE RECHERCHE D'HALITE
 		Command _search_halite() {
-
-
+			// Recherche parmis les cases voisines si il y'a plus d'halite 
 			int maxHalite = 0;
-
+			// Par défaut, la direction est null
 			Direction dirMaxHalite = Direction::STILL;
+			// Ne reste pas fixe si il y a un ennemi proche
 			bool forceMove = false;
+			// Parcours en diamant des cases voisines
 			for (int x = -1; x <= 1; ++x) {
 				for (int y = -(1 - abs(x)); y <= (1 - abs(x)); ++y) {
+					// Position de la case adjacente a tester
 					Position p = ship->position + Position(x, y);
 					Direction d = game_map->naive_navigate(ship, p);
 					if (d != Direction::STILL && game_map->at(p)->halite > maxHalite) {
 						if (enemyShipsInRange(p, 1, ship->owner, game_map).size() > 0) forceMove = true;
+						// Si la case est atteignable et qu'il n'y a pas d'ennemi proche on regarde si elle a plus d'halite que les autres
 						else {
 							dirMaxHalite = d;
 							maxHalite = game_map->at(p)->halite;
@@ -176,16 +174,17 @@ namespace shipBTree{
 					
 				}
 			}
+			// Si la case est vidé et qu'il y'a des cases autour plus rentable le vaisseau se déplace
 			if ((maxHalite > 0 && forceMove) || game_map->at(ship)->halite < constants::MAX_HALITE / 10 && (maxHalite > game_map->at(ship)->halite || game_map->at(ship)->has_structure())) {
 				return ship->move(dirMaxHalite);
 
 			}
-
+			// Sinon il reste
 			return ship->stay_still();
 
 		}
 		
-
+		// ACTION DE RECOLTE
 		Command _recolt() {
 
 			return ship->stay_still();
